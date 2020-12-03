dc854908c10b022e2594292136b624a23a0c970a
Yum-git <yzk.yuma@gmail.com>
2020-12-03 15:35:31 +0900

    fix mockup add

--- main.py
+++ main.py
@@ -6,7 +6,14 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     logs = ['20201123 : saoshoahsoa', '20201123 : ahara is xss ']
-    return render_template("index.html", logs=logs)
+    status_all = [['SQL', 'カスタム'], ['RFI', 'とてもきつい'], ['TRAVERSAL', 'きつい'], ['EVADE', 'ゆるい'], ['XSS', 'とてもゆるい']]
+
+    return render_template("index.html", logs=logs, status_all=status_all)
+
+
+@app.route("/api")
+def api():
+    return 'a'
 
 
 @app.route("/favicon.ico")
