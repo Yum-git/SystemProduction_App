7f547fad0fa3c7e0d5e75d71f99e321033b3ba1a
Yum-git <yzk.yuma@gmail.com>
2020-12-25 15:30:02 +0900

    fix　デザイン

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import crossplane
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    logs = ['20201123 : saoshoahsoa', '20201123 : ahara is xss ']
+    logs = ['20201123 : saoshoahsoa' for i in range(100)]
     status_all = [['SQL', 'custom'], ['RFI', 'very_hard'], ['TRAVERSAL', 'hard'], ['EVADE', 'soft'], ['XSS', 'very_soft']]
 
     return render_template("index.html", logs=logs, status_all=status_all)
 
 
-@app.route("/api")
+@app.route("/api", methods=['POST'])
 def api():
+    print(request.get_data().decode())
+    request_data = list(request.get_data().decode().split('='))
+    print(request_data)
 
-    return 'a'
+    print(crossplane.parse('/usr/local/nginx/conf/nginx.conf'))
+    return request.form["sql"]
 
 
 @app.route("/favicon.ico")
